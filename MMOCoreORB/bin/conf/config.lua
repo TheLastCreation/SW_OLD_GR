890a9deb0728e4e8253b97565c037479463e754f
grserver <grserver@users.noreply.github.com>
2015-01-17 10:08:06 +0000

    Update config.lua

--- MMOCoreORB/bin/conf/config.lua
+++ MMOCoreORB/bin/conf/config.lua
@@ -194,7 +194,8 @@ TreFiles = {
 	"gr_publish_01_03.tre",
 	"gr_publish_02_01.tre",
 	"gr_publish_04_01.tre",
-	"gr_publish_04_02.tre"
+	"gr_publish_04_02.tre",
+	"gr_publish_05_01.tre"
 }
 
 --Status Server Config
