fb45feac51724fb8795629c03b945319825c8944
grserver <grserver@gmail.com>
2015-01-21 08:46:16 +0000

    [Fixed] Typo

--- MMOCoreORB/bin/scripts/screenplays/gr_screenplays/lok/kill_the_general.lua
+++ MMOCoreORB/bin/scripts/screenplays/gr_screenplays/lok/kill_the_general.lua
@@ -59,39 +59,9 @@ npcMapKillTheGeneral =
 			spawnData = { planetName = "naboo", npcTemplate = "ktg_palpatine_hologram", x = 15.3334, z = -13.7584, y = -15.7758, direction = -8, cellID = 1260046, position = STAND },
 			worldPosition = { x = 5386, y = 6405 },
 			npcNumber = 1,
-			stfFile = "@gr_themepark/test_convo.stf",
+			stfFile = "@gr_themepark/test_convo",
 			missions = ktg_palpatine_hologram_missions
 		},
-		--[[{
-			spawnData = { planetName = "tatooine", npcTemplate = "ree_yees", x = 5.97, z = 0.299998, y = 116.1, direction = 248.813, cellID = 1177466, position = STAND },
-			worldPosition = { x = -5892.3, y = -6230.8 },
-			npcNumber = 2,
-			stfFile = "@theme_park_jabba/ree_yees",
-			missions = ree_yees_missions
-		},
-		{
-			spawnData = { planetName = "tatooine", npcTemplate = "ephant_mon", x = -6, z = 5.85, y = 84, direction = 277.481, cellID = 1177469, position = STAND },
-			worldPosition = { x = -5924.4, y = -6242.4 },
-			npcNumber = 4,
-			stfFile = "@theme_park_jabba/ephant_mon",
-			missions = ephant_mon_missions
-		},
-		{
-			spawnData = { planetName = "tatooine", npcTemplate = "porcellus", x = -43, z = 3, y = 64, direction = 306.623, cellID = 1177477, position = STAND },
-			worldPosition = { x = -5963.5, y = -6226.9 },
-			npcNumber = 8,
-			stfFile = "@theme_park_jabba/porcellus",
-			missions = porcellus_missions
-		},
-		{
-			spawnData = { planetName = "tatooine", npcTemplate = "barada", x = 30.57, z = 0.199999, y = -1.05, direction = 164.173, cellID = 1177499, position = STAND },
-			worldPosition = { x = -5965.8, y = -6325.1 },
-			npcNumber = 16,
-			stfFile = "@theme_park_jabba/barada",
-			missions = barada_missions
-		},]]--
-
-
 	}
 
 KillTheGeneral = ThemeParkLogic:new {
