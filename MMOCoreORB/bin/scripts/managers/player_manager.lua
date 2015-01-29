7681f43843fc64a8364b903af29918715392a90f
terminator2014 <terminatorgithub@hotmail.com>
2015-01-29 11:11:37 +0000

    {Changed amount of vehicles on self}
    {Changed amount of vehicles on self}

--- MMOCoreORB/bin/scripts/managers/player_manager.lua
+++ MMOCoreORB/bin/scripts/managers/player_manager.lua
@@ -61,7 +61,7 @@ globalExpMultiplier = 10.1
 baseStoredCreaturePets = 2
 baseStoredFactionPets = 3
 baseStoredDroids = 5
-baseStoredVehicles = 3
+baseStoredVehicles = 5
 baseStoredShips = 3
 
 -----------------------------------
