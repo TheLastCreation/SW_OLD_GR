11de15318705b5a74f8972d3efd976065e9e4cea
Lasko-GR <lasko.swggr@gmail.com>
2015-02-19 16:52:48 +0000

    [Added] More CU/NGE Mobs
    Basic templates/objects that need editing

--- MMOCoreORB/bin/scripts/object/building/player/atat_house.lua
+++ MMOCoreORB/bin/scripts/object/building/player/atat_house.lua
@@ -15,7 +15,7 @@ object_building_player_atat_house = object_building_player_shared_atat_house:new
 			{templateFile = "object/tangible/terminal/terminal_player_structure.iff", x = -7.63883, z = 15.5055, y = 25.5973, ow = 0.522499, ox = 0, oz = 0, oy = 0.85264, cellid = 2, containmentType = -1}
 --			{templateFile = "object/tangible/terminal/terminal_elevator_up.iff", x = -2.66109, z = -0.513074, y = 0.0544102, ow = -0.707107, ox = 0, oz = 0, oy = 0.707107, cellid = 7, containmentType = -1},
 --			{templateFile = "object/tangible/terminal/terminal_elevator_down.iff", x = -2.65908, z = 5.39344, y = -0.0155522, ow = -0.707107, ox = 0, oz = 0, oy = 0.707107, cellid = 7, containmentType = -1}
-	}
+	},
 	constructionMarker = "object/building/player/construction/construction_player_house_corellia_large_style_01.iff"
 }
 
