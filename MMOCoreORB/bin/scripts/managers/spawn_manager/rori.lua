77ad8f150a79c969f619a189a8f137cdd0259830
terminator2014 <terminatorgithub@hotmail.com>
2015-03-01 21:08:29 +0000

    {Added squad leader trainer to rori cantina}
    plus changed mobs in bunker

--- MMOCoreORB/bin/scripts/managers/spawn_manager/rori.lua
+++ MMOCoreORB/bin/scripts/managers/spawn_manager/rori.lua
@@ -201,6 +201,7 @@ rori_static_spawns = {
 	{"junk_dender",60,-5428.2,80,-2244.2,10,0, "", "Dender", "stationary",JUNKGUNGAN,JUNKCONVDENDERRORI},
 	{"junk_dealer",60,5353.8,80,5665.9,180,0, "", "", "stationary",JUNKGENERIC,JUNKCONVGENERIC},
 	{"junk_dealer",60,5350.2,81.3,5484.8,-50,0, "", "", "stationary",JUNKGENERIC,JUNKCONVGENERIC},
+	{"trainer_squadleader",60, -9.2, 0.6, 3.7, 83, 4505635, "conversation", "", "stationary"},
 }
 
 rori_badges = {
