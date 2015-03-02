f8a111da4d6aeee90b92b0957346bc944ceba09b
terminator2014 <terminatorgithub@hotmail.com>
2015-03-02 16:35:16 +0000

    visibility chance

--- MMOCoreORB/src/server/zone/managers/visibility/VisibilityManager.cpp
+++ MMOCoreORB/src/server/zone/managers/visibility/VisibilityManager.cpp
@@ -98,12 +98,12 @@ float VisibilityManager::calculateVisibilityIncrease(CreatureObject* creature) {
 				ManagedReference<CreatureObject*> c = cast<CreatureObject*>(obj);
 				if (c->isNonPlayerCreatureObject()) {
 					if (creature->getFaction() == 0 || (c->getFaction() != factionImperial && c->getFaction() != factionRebel)) {
-						visibilityIncrease += 0.5;
+						visibilityIncrease += 1.0;
 					} else {
 						if (creature->getFaction() == c->getFaction()) {
-							visibilityIncrease += 0.5;
+							visibilityIncrease += 1.0;
 						} else {
-							visibilityIncrease += 0.5;
+							visibilityIncrease += 1.0;
 						}
 					}
 				}
