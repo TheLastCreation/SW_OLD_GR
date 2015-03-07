f9bdc42b998e41dda6ad7747b6e70e8c65c3d63d
terminator2014 <terminatorgithub@hotmail.com>
2015-03-07 01:45:07 +0000

    {Edited ns_clothing group}
    type 3 shirt and hat dosnt work for some reason

--- MMOCoreORB/bin/scripts/loot/groups/swggr_custom/ns_clothing.lua
+++ MMOCoreORB/bin/scripts/loot/groups/swggr_custom/ns_clothing.lua
@@ -8,13 +8,11 @@ ns_clothing = {
 		{itemTemplate = "ns_boots", weight = 1000000},
 		{itemTemplate = "ns_dress", weight = 1000000},
 		{itemTemplate = "ns_hat_type_01", weight = 1000000},
-		{itemTemplate = "ns_hat_type_02", weight = 1000000},
-		{itemTemplate = "ns_hat_type_03", weight = 1000000},
+		{itemTemplate = "ns_hat_type_02", weight = 2000000},
 		{itemTemplate = "ns_pants_type_01", weight = 1000000},
 		{itemTemplate = "ns_pants_type_02", weight = 1000000},
 		{itemTemplate = "ns_shirt_type_01", weight = 1000000},
-		{itemTemplate = "ns_shirt_type_02", weight = 1000000},
-		{itemTemplate = "ns_shirt_type_03", weight = 1000000}
+		{itemTemplate = "ns_shirt_type_02", weight = 2000000}
 	}
 }
 
