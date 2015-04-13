efb2053f19efe0f6fdd5da91a4dd8931b2ef256b
terminator2014 <terminatorgithub@hotmail.com>
2015-04-13 11:36:21 +0100

    {Fix} Typo
    taken out experimental

--- MMOCoreORB/bin/scripts/object/tangible/tcg/series8/vader_pod.lua
+++ MMOCoreORB/bin/scripts/object/tangible/tcg/series8/vader_pod.lua
@@ -43,16 +43,6 @@
 
 object_tangible_tcg_series8_vader_pod = object_tangible_tcg_series8_vader_pod:new {
 
-
-	numberExperimentalProperties = {1, 1, 1, 2},
-	experimentalProperties = {"XX", "XX", "XX", "DR", "OQ"},
-	experimentalWeights = {1, 1, 1, 1, 1},
-	experimentalGroupTitles = {"null", "null", "null", "exp_quality"},
-	experimentalSubGroupTitles = {"null", "null", "hitpoints", "quality"},
-	experimentalMin = {0, 0, 1000, 1},
-	experimentalMax = {0, 0, 1000, 100},
-	experimentalPrecision = {0, 0, 0, 0},
-	experimentalCombineType = {0, 0, 4, 1},
 }
 
 ObjectTemplates:addTemplate(object_tangible_tcg_series8_vader_pod, "object/tangible/tcg/series8/vader_pod.iff")
