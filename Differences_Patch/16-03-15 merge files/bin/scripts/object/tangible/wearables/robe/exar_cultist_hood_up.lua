02bd9691b971c4621988b4b68a08a35d847e4c85
Lasko-GR <lasko.swggr@gmail.com>
2015-04-15 12:02:04 +0100

    [Added] Scripts for Armour
    And the 400th commit is mine:)

--- Differences_Patch/16-03-15 merge files/bin/scripts/object/tangible/wearables/robe/exar_cultist_hood_up.lua
+++ Differences_Patch/16-03-15 merge files/bin/scripts/object/tangible/wearables/robe/exar_cultist_hood_up.lua
@@ -25,10 +25,10 @@ object_tangible_wearables_robe_exar_cultist_hood_up = object_tangible_wearables_
 				    {"jedi_force_power_regen", 10}
 				},
 
-				noTrade = 0,
+				noTrade = 1,
 
 				objectMenuComponent = {"cpp", "RobeObjectMenuComponent"},
-				certificationsRequired = {"force_title_jedi_rank_02"},
+				certificationsRequired = {"force_title_jedi_rank_10"},
 				attributeListComponent = "JediRobeAttributeListComponent",
 }
 
--- Differences_Patch/16-03-15 merge files/bin/scripts/object/tangible/wearables/robe/robe_chronicle_master.lua
+++ Differences_Patch/16-03-15 merge files/bin/scripts/object/tangible/wearables/robe/robe_chronicle_master.lua
@@ -20,16 +20,11 @@ object_tangible_wearables_robe_robe_chronicle_master = object_tangible_wearables
 				"object/creature/player/zabrak_male.iff",
 				"object/creature/player/zabrak_female.iff" },
 				
-				skillMods = {
-				    {"jedi_force_power_max", 250},
-				    {"jedi_force_power_regen", 10}
-				},
+				skillMods = {},
 
-				noTrade = 0,
+				noTrade = 1,
 
-				objectMenuComponent = {"cpp", "RobeObjectMenuComponent"},
-				certificationsRequired = {"force_title_jedi_rank_02"},
-				attributeListComponent = "JediRobeAttributeListComponent",
+
 }
 
 ObjectTemplates:addTemplate(object_tangible_wearables_robe_robe_chronicle_master, "object/tangible/wearables/robe/robe_chronicle_master.iff")
