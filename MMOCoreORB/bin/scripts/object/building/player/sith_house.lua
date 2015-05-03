be65535c2d9e2e065b9fb3971cc53e44764965ef
terminator2014 <terminatorgithub@hotmail.com>
2015-05-03 17:20:33 +0100

    {Added Sith house yo yavin4}

--- MMOCoreORB/bin/scripts/object/building/player/sith_house.lua
+++ MMOCoreORB/bin/scripts/object/building/player/sith_house.lua
@@ -1,15 +1,15 @@
 object_building_player_sith_house = object_building_player_shared_sith_house:new {
-	lotSize = 2,
+	lotSize = 1,
 	baseMaintenanceRate = 10,
-	allowedZones = {"corellia", "dantooine", "lok", "naboo", "rori", "talus", "tatooine", "taanab", "dathomir"},
+	allowedZones = {"corellia", "dantooine", "lok", "naboo", "rori", "talus", "tatooine", "taanab", "dathomir", "yavin4"},
 	constructionMarker = "object/building/player/construction/construction_player_house_corellia_large_style_01.iff",
 	length = 1,
 	width = 1,
 	publicStructure = 0,
 	skillMods = {
-		{"private_medical_rating", 100},
-		{"private_buff_mind", 100},
-		{"private_med_battle_fatigue", 15}
+		{"private_medical_rating", 500},
+		{"private_buff_mind", 500},
+		{"private_med_battle_fatigue", 150}
 	},
 	childObjects = {
 --			{templateFile = "object/tangible/sign/player/house_address.iff", x = 4.34, z = 3.4, y = 18.40, ox = 0, oy = 0.707107, oz = 0, ow = 0.707107, cellid = -1, containmentType = -1},
