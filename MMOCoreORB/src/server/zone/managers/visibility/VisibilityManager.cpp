d35922ea62285a189bd6e0aa8908d30c8274c4f8
terminator2014 <terminatorgithub@hotmail.com>
2015-05-08 21:07:53 +0100

    {Edited visibility range}
    {Edited visibility range}

--- MMOCoreORB/src/server/zone/managers/visibility/VisibilityManager.cpp
+++ MMOCoreORB/src/server/zone/managers/visibility/VisibilityManager.cpp
@@ -47,14 +47,14 @@ float VisibilityManager::calculateVisibilityIncrease(CreatureObject* creature) {
 		SortedVector<ManagedReference<QuadTreeEntry* > > closeObjects;
 		CloseObjectsVector* closeObjectsVector = (CloseObjectsVector*) creature->getCloseObjects();
 		if (closeObjectsVector == NULL) {
-			zone->getInRangeObjects(creature->getWorldPositionX(), creature->getWorldPositionY(), 45, &closeObjects, true);
+			zone->getInRangeObjects(creature->getWorldPositionX(), creature->getWorldPositionY(), 35, &closeObjects, true);
 		} else {
 			closeObjectsVector->safeCopyTo(closeObjects);
 		}
 
 		for (int i = 0; i < closeObjects.size(); ++i) {
 			SceneObject* obj = cast<SceneObject*>(closeObjects.get(i).get());
-			if (obj->isCreatureObject() && creature->isInRange(obj, 45)) {
+			if (obj->isCreatureObject() && creature->isInRange(obj, 35)) {
 				ManagedReference<CreatureObject*> c = cast<CreatureObject*>(obj);
 				if (c->isNonPlayerCreatureObject()) {
 					if (creature->getFaction() == 0 || (c->getFaction() != factionImperial && c->getFaction() != factionRebel)) {
