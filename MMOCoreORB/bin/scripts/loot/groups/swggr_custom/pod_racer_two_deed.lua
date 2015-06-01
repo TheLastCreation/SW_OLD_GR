b609a7fcd2711efed0534f68d217905cbebb26f4
terminator2014 <terminatorgithub@hotmail.com>
2015-06-01 19:48:12 +0100

    {Edited} loot drop
    made server crash

---
+++ MMOCoreORB/bin/scripts/loot/groups/swggr_custom/pod_racer_two_deed.lua
@@ -0,0 +1,10 @@
+pod_racer_two_deed = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+		{itemTemplate = "pod_racer_two_deed", weight = 10000000}
+	}
+}
+
+addLootGroupTemplate("pod_racer_two_deed", pod_racer_two_deed)
---
+++ MMOCoreORB/bin/scripts/loot/items/swggr_custom/vehicle_deeds/pod_racer_two_deed.lua
@@ -0,0 +1,11 @@
+pod_racer_two_deed = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "Pod_Racer_One",
+	directObjectTemplate = "object/tangible/deed/vehicle_deed/pod_racer_two_deed.iff",
+	craftingValues = {
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+addLootItemTemplate("pod_racer_two_deed", pod_racer_two_deed)
