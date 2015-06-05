6d34a15a3a93b2f1903da1e12b9211f3e440224d
grserver <gallacticrebellion.emu@gmail.com>
2015-06-05 15:24:32 +0100

    [Fixed] TYPO

--- MMOCoreORB/bin/conf/config.lua
+++ MMOCoreORB/bin/conf/config.lua
@@ -140,8 +140,15 @@ ZonesEnabled = {
 TrePath = "/home/swgemu/workspace/tre"
 
 TreFiles = {
+	"ghomrassen.tre",
 	"gr_publish_05_01.tre",
+	"gr_publish_04_02.tre",
+	"gr_publish_04_01.tre",
 	"gr_publish_03_01.tre",
+	"gr_publish_02_01.tre",
+	"gr_publish_01_03.tre",
+	"gr_publish_01_02.tre",
+	"gr_publish_01_01.tre",
 	"default_patch.tre",
 	"patch_sku1_14_00.tre",
 	"patch_14_00.tre",
@@ -193,13 +200,6 @@ TreFiles = {
 	"data_sample_00.tre",
 	"data_music_00.tre",
 	"bottom.tre",
-	"gr_publish_01_01.tre",
-	"gr_publish_01_02.tre",
-	"gr_publish_01_03.tre",
-	"gr_publish_02_01.tre",
-	"gr_publish_04_01.tre",
-	"gr_publish_04_02.tre",
-	"ghomrassen.tre"
 }
 
 --Status Server Config
