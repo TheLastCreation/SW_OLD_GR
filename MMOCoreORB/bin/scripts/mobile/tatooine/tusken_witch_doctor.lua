70d44306ede4bf936fb3c6b32a80272d46b633c0
terminator2014 <terminatorgithub@hotmail.com>
2015-06-09 11:17:13 +0100

    [Edited] Tusken witch loot
    changed loot drop and %

--- MMOCoreORB/bin/scripts/mobile/tatooine/tusken_witch_doctor.lua
+++ MMOCoreORB/bin/scripts/mobile/tatooine/tusken_witch_doctor.lua
@@ -32,13 +32,14 @@ tusken_witch_doctor = Creature:new {
 				{group = "task_loot_medallion_dark_jedi", chance = 1500000},
 				{group = "pearls_flawless", chance = 3000000},
 				{group = "wearables_scarce", chance = 2000000},
-				{group = "holocron_light", chance = 750000},
+				{group = "g_bespin_house_loot_deed", chance = 750000},
 				{group = "holocron_dark", chance = 750000},
 				{group = "armor_attachments", chance = 500000},
 				{group = "clothing_attachments", chance = 500000},
 				{group = "color_crystals", chance = 500000},
 				{group = "crystals_premium", chance = 500000}
-			}
+			},
+			lootChance = 9000000
 		}
 	},
 	weapons = {"tusken_weapons"},
