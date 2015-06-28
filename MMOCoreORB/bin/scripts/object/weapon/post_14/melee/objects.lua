32baeb15006cfcdb30aea91f8e29c142478d5671
lasko2112 <laskorunningflier@gmail.com>
2015-06-28 12:40:51 +0000

    [Fixed] Gen 5 sabers

--- MMOCoreORB/bin/scripts/object/weapon/post_14/melee/objects.lua
+++ MMOCoreORB/bin/scripts/object/weapon/post_14/melee/objects.lua
@@ -592,13 +592,7 @@ object_weapon_melee_2h_sword_crafted_saber_shared_sword_lightsaber_two_handed_ge
 
 ObjectTemplates:addClientTemplate(object_weapon_melee_2h_sword_crafted_saber_shared_sword_lightsaber_two_handed_gen4_must, "object/weapon/melee/2h_sword/crafted_saber/shared_sword_lightsaber_two_handed_gen4_must.iff")
 --****************************************************************************************************************************************************************************
-object_weapon_melee_2h_sword_crafted_saber_shared_sword_lightsaber_two_handed_gen5 = SharedWeaponObjectTemplate:new {
-	clientTemplateFileName = "object/weapon/melee/2h_sword/crafted_saber/shared_sword_lightsaber_two_handed_gen5.iff"
 
-}
-
-ObjectTemplates:addClientTemplate(object_weapon_melee_2h_sword_crafted_saber_shared_sword_lightsaber_two_handed_gen5, "object/weapon/melee/2h_sword/crafted_saber/shared_sword_lightsaber_two_handed_gen5.iff")
---****************************************************************************************************************************************************************************
 object_weapon_melee_2h_sword_crafted_saber_shared_sword_lightsaber_two_handed_gen5_eow = SharedWeaponObjectTemplate:new {
 	clientTemplateFileName = "object/weapon/melee/2h_sword/crafted_saber/shared_sword_lightsaber_two_handed_gen5_eow.iff"
 
