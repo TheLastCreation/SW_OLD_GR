4e9eaf5213e49caedf1e38f4e774747ef5cbfe9b
terminator2014 <terminatorgithub@hotmail.com>
2015-07-29 14:37:21 +0100

    [WIP] kass mobs
    In Temple behind prision

--- MMOCoreORB/bin/scripts/screenplays/static_spawns/kaas_static_spawns.lua
+++ MMOCoreORB/bin/scripts/screenplays/static_spawns/kaas_static_spawns.lua
@@ -16,7 +16,9 @@ end
 
 function KaasStaticSpawnsScreenPlay:spawnMobiles()
 	
-	--
+	--Kass Temple
+	spawnMobile("kaas", "exar_kun", 200,  -01, -37.3, 17.2, 0, 35791397)
+	spawnMobile("kaas", "exar_kun_cultist_01", 200,  39, -37.3, 14.7, 0, 35791397)
 	
 	
 	
