a0d65d36fd371333df8b5478caa25a07d790bad1
terminator2014 <terminatorgithub@hotmail.com>
2015-08-08 12:42:58 +0100

    [Edited] yavin4
    planet manager

--- MMOCoreORB/bin/scripts/managers/planet_manager.lua
+++ MMOCoreORB/bin/scripts/managers/planet_manager.lua
@@ -249,7 +249,7 @@ yavin4 = {
 	planetTravelPoints = {
 		{name = "Yavin IV Labor Outpost", x = -6921.6733, z = 73, y = -5726.5161, interplanetaryTravelAllowed = 1, incomingTravelAllowed = 1},
 		{name = "Yavin IV Mining Outpost", x = -267.23914, z = 35, y = 4896.3013, interplanetaryTravelAllowed = 1, incomingTravelAllowed = 1},
-		{name = "Yavin IV Imperial Outpost", x = 4054.1, z = 37, y = -6216.9, interplanetaryTravelAllowed = 1, incomingTravelAllowed = 1},
+		{name = "Yavin IV Imperial Outpost", x = 4054.1, z = 37, y = -6216.9, interplanetaryTravelAllowed = 1, incomingTravelAllowed = 1}
 	},
 	
 	planetObjects = {
