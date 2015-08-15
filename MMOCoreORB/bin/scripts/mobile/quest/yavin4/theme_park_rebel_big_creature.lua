d74a041f43fee0399d2d3cd894e87a9c78d7f984
terminator2014 <terminatorgithub@hotmail.com>
2015-08-15 12:56:40 +0100

    [Merge] From Unstable
    [removed] DEFAULT 'attack' command from invading the combat-queue,
    Mantis 6522
    
    Change-Id: I250e9c52595ad4d923776551c25e8d697fca5dbe
    Aug 15, 2015 9:20 AM

--- MMOCoreORB/bin/scripts/mobile/quest/yavin4/theme_park_rebel_big_creature.lua
+++ MMOCoreORB/bin/scripts/mobile/quest/yavin4/theme_park_rebel_big_creature.lua
@@ -24,7 +24,8 @@ theme_park_rebel_big_creature = Creature:new {
 	creatureBitmask = PACK + HERD,
 	optionsBitmask = 128,
 	diet = CARNIVORE,
-
+    scale = 1.5,
+	
 	templates = {"object/mobile/mawgax_hue.iff"},
 	lootGroups = {
 		{
