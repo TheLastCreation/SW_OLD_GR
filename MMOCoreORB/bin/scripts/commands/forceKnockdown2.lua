64ade48979f3230a5558b7c58df5dfe091285e9b
terminator2014 <terminatorgithub@hotmail.com>
2015-08-23 15:35:56 +0100

    [Merge] From Unstable
    [Fixed] creature special attack dots now apply to the pool that was
    damaged with the attack
    [Changed] blue frog applied dots now apply to a random pool
    
    Change-Id: Ic0210b571702f703b90dc9cbc138367f308300a0
    Aug 22, 2015 9:41 AM

--- MMOCoreORB/bin/scripts/commands/forceKnockdown2.lua
+++ MMOCoreORB/bin/scripts/commands/forceKnockdown2.lua
@@ -44,7 +44,7 @@
 ForceKnockdown2Command = {
         name = "forceknockdown2",
 
-    damage = 200,
+    damage = 0,
 	speed = 4.0,
 	forceCost = 100,
 	accuracySkillMod = "forceknockdown_accuracy";
