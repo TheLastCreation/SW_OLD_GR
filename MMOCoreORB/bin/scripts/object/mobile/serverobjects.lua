b4334bd922bf0f7c56c9af98c6427f2be0986f68
terminator2014 <terminatorgithub@hotmail.com>
2015-09-09 18:25:48 +0100

    [Added] gen to shared

--- MMOCoreORB/bin/scripts/object/mobile/serverobjects.lua
+++ MMOCoreORB/bin/scripts/object/mobile/serverobjects.lua
@@ -87,6 +87,7 @@ includeFile("mobile/outbreak_undead_deathtrooper_28_m.lua")
 
 includeFile("mobile/watto.lua")
 includeFile("mobile/yoda.lua")
+includeFile("mobile/general_grievous.lua")
 
 
 -- Server Objects
