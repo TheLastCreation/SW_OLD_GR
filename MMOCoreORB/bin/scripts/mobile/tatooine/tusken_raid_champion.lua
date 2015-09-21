d2d0d80253d68af90af31f9193d563a7ea4fc04e
terminator2014 <terminatorgithub@hotmail.com>
2015-09-21 23:26:19 +0100

    [Fixed] More Unit Tests
    Misc

--- MMOCoreORB/bin/scripts/mobile/tatooine/tusken_raid_champion.lua
+++ MMOCoreORB/bin/scripts/mobile/tatooine/tusken_raid_champion.lua
@@ -31,13 +31,13 @@ tusken_raid_champion = Creature:new {
 			groups = {
 				{group = "junk", chance = 1500000},
 				{group = "tusken_common", chance = 3000000},
-				{group = "wearables_all", chance = 2000000},
+				{group = "wearables_common", chance = 500000},
 				{group = "bone_armor", chance = 750000},
 				{group = "chitin_armor", chance = 750000},
 				{group = "armor_attachments", chance = 500000},
 				{group = "clothing_attachments", chance = 500000},
-				{group = "color_crystals", chance = 500000},
-				{group = "crystals_good", chance = 500000}
+				{group = "color_crystals", chance = 2000000},
+				{group = "crystals_poor", chance = 500000}
 			}
 		}
 	},
--- MMOCoreORB/bin/scripts/mobile/tatooine/tusken_raider.lua
+++ MMOCoreORB/bin/scripts/mobile/tatooine/tusken_raider.lua
@@ -31,12 +31,12 @@ tusken_raider = Creature:new {
 			groups = {
 				{group = "junk", chance = 1500000},
 				{group = "tusken_common", chance = 3000000},
-				{group = "wearables_common", chance = 2000000},
+				{group = "wearables_common", chance = 500000},
 				{group = "bone_armor", chance = 750000},
 				{group = "chitin_armor", chance = 750000},
 				{group = "armor_attachments", chance = 500000},
 				{group = "clothing_attachments", chance = 500000},
-				{group = "color_crystals", chance = 500000},
+				{group = "color_crystals", chance = 2000000},
 				{group = "crystals_poor", chance = 500000}
 			}
 		}
