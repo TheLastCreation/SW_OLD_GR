4c1f59fc8b5bc6ae47e84066e4d3497a6b79d2e3
terminator2014 <terminator2014@hotmail.com>
2015-09-21 14:18:29 +0100

    [Fixed] Unit test

--- MMOCoreORB/bin/scripts/mobile/conversations.lua
+++ MMOCoreORB/bin/scripts/mobile/conversations.lua
@@ -141,6 +141,8 @@ includeFile("conversations/racetracks/mosespaconversation.lua")
 includeFile("conversations/racetracks/narmleconversation.lua")
 includeFile("conversations/racetracks/lokconversation.lua")
 includeFile("conversations/racetracks/nashalconversation.lua")
+includeFile("conversations/racetracks/mosrassenracecoordinator .lua")
+
 
 -- Space
 includeFile("conversations/space/chassis_dealer_conv.lua")
